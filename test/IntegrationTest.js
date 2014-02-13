6c00d6c2789d54bbf788fd37a1fddcc55cc437a1
Terrence Howard <chemisus@gmail.com>
2014-02-12 21:10:02 -0500

    can do a subquery

--- test/IntegrationTest.js
+++ test/IntegrationTest.js
@@ -16,7 +16,8 @@ describe('integration test', function () {
         eq: new EqualOperation(),
         or: new OrOperation(),
         and: new AndOperation(),
-        not: new NotOperation()
+        not: new NotOperation(),
+        true: new TrueOperation()
     };
 
     var environment = {
@@ -80,6 +81,38 @@ describe('integration test', function () {
 
         var data = ops.select.make(
             ops.table.make('users'),
+            ops.and.make([
+                ops.not.make(
+                    ops.eq.make(
+                        ops.get.make('name'),
+                        ops.const.make('e')
+                    )
+                ),
+                ops.eq.make(
+                    ops.get.make('type'),
+                    ops.const.make(2)
+                )
+            ])
+        );
+
+        expect(ops[data[0]].execute(data, environment)).toEqual([{name: 'd', type: 2}, {name: 'f', type: 2}]);
+    });
+
+    it('should be able to do a subquery', function () {
+        tables.users = [
+            {name: 'a', type: 1},
+            {name: 'b', type: 1},
+            {name: 'c', type: 1},
+            {name: 'd', type: 2},
+            {name: 'e', type: 2},
+            {name: 'f', type: 2},
+        ];
+
+        var data = ops.select.make(
+            ops.select.make(
+                ops.table.make('users'),
+                ops.true.make()
+            ),
             ops.and.make([
                 ops.not.make(
                     ops.eq.make(
