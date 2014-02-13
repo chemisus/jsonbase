8c17f634cd8e48ae14d6e4a5dd150068e978b7c1
Terrence Howard <chemisus@gmail.com>
2014-02-12 20:37:29 -0500

    added test for falsy

--- test/Operations/OrOperationTest.js
+++ test/Operations/OrOperationTest.js
@@ -31,4 +31,30 @@ describe('or operation', function () {
         expect(environment.operations.c.execute).toHaveBeenCalled();
         expect(environment.operations.d.execute).not.toHaveBeenCalled();
     });
+
+    it('should return false if no operation returns true', function () {
+        var environment = {
+            operations: {
+                a: jasmine.createSpyObj('a', ['execute']),
+                b: jasmine.createSpyObj('b', ['execute']),
+                c: jasmine.createSpyObj('c', ['execute']),
+                d: jasmine.createSpyObj('d', ['execute'])
+            }
+        };
+
+        var values = [['a'], ['b'], ['c'], ['d']];
+        var data = operation.make(values);
+
+        environment.operations.a.execute.andReturn(false);
+        environment.operations.b.execute.andReturn(false);
+        environment.operations.c.execute.andReturn(false);
+        environment.operations.d.execute.andReturn(false);
+
+        expect(operation.execute(data, environment)).toBeFalsy();
+
+        expect(environment.operations.a.execute).toHaveBeenCalled();
+        expect(environment.operations.b.execute).toHaveBeenCalled();
+        expect(environment.operations.c.execute).toHaveBeenCalled();
+        expect(environment.operations.d.execute).toHaveBeenCalled();
+    });
 });
