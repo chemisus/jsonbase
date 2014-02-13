17a56cadea74ca2ad3b52d746bd478e5d165ea12
Terrence Howard <chemisus@gmail.com>
2014-02-12 21:02:11 -0500

    added test for not equal

--- test/Operations/EqualOperationTest.js
+++ test/Operations/EqualOperationTest.js
@@ -25,4 +25,23 @@ describe('equal operation', function () {
         expect(environment.operations.lhs.execute).toHaveBeenCalled();
         expect(environment.operations.rhs.execute).toHaveBeenCalled();
     });
+
+    it('should return false if the values are not equal', function () {
+        var environment = {
+            operations: {
+                lhs: jasmine.createSpyObj('lhs', ['execute']),
+                rhs: jasmine.createSpyObj('rhs', ['execute'])
+            }
+        };
+
+        var data = operation.make(['lhs'], ['rhs']);
+
+        environment.operations.lhs.execute.andReturn(4);
+        environment.operations.rhs.execute.andReturn(5);
+
+        expect(operation.execute(data, environment)).toBeFalsy();
+
+        expect(environment.operations.lhs.execute).toHaveBeenCalled();
+        expect(environment.operations.rhs.execute).toHaveBeenCalled();
+    });
 });
